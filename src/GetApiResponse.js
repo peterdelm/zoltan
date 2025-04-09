e3f1ec679cacf7a75b0b511d2a03571f359a086f
peterdelm <peterdelmastro@hotmail.com>
2025-04-08 23:07:41 -0400

    temp repair for failed api

--- src/GetApiResponse.js
+++ src/GetApiResponse.js
@@ -32,6 +32,29 @@ const GetApiResponse = ({ showMaze, setEntryId }) => {
     setUserInput("");
     setUserName("");
 
+    ///////  API ENDPOINT APPEARS TO BE DOWN, USING LOCAL QUOTE LIST UNTIL IT COMES BACK ///////
+
+    // if (query !== "") {
+    //   console.log("Calling API...");
+    // API call to return advice for keyword input
+    //   axios({
+    //     // url: `https://api.adviceslip.com/advice/search/${query}`,
+    //     url: `https://api.quotable.io/random`,
+    //     method: "GET",
+    //     dataResponse: "json",
+    //   }).then((res) => {
+    //     const data = res.data;
+    //     console.log("Data is", data);
+    //     // Check if data has object of slips
+    //     if (data.hasOwnProperty("slips")) {
+    //       setAdvice(res.data.slips[0].advice);
+    //     } else {
+    //       // Second API call to return advice for words not in firs API data base
+    //       randomAdvice(setAdvice);
+    //     }
+    //   });
+    // }
+
     const newUser = {
       name: userName,
       input: advice,
@@ -52,21 +75,33 @@ const GetApiResponse = ({ showMaze, setEntryId }) => {
 
   useEffect(() => {
     if (query !== "") {
-      // API call to return advice for keyword input
-      axios({
-        url: `https://api.adviceslip.com/advice/search/${query}`,
-        method: "GET",
-        dataResponse: "json",
-      }).then((res) => {
-        const data = res.data;
-        // Check if data has object of slips
-        if (data.hasOwnProperty("slips")) {
-          setAdvice(res.data.slips[0].advice);
-        } else {
-          // Second API call to return advice for words not in firs API data base
-          randomAdvice(setAdvice);
-        }
-      });
+      console.log("Fetching random advice...");
+
+      // Use a list of random advice as fallback
+      const adviceList = [
+        "Don’t be afraid to start over. It’s a chance to build something better.",
+        "Success is a journey, not a destination. Enjoy the ride.",
+        "Take the time to invest in yourself. It’s the best decision you can make.",
+        "It’s okay to not have everything figured out yet. Take it one step at a time.",
+        "You’re stronger than you think. Keep pushing through the challenges.",
+        "Sometimes, the best thing you can do is take a break and come back with fresh eyes.",
+        "If you want to go fast, go alone. If you want to go far, go together.",
+        "Surround yourself with people who lift you up, not bring you down.",
+        "Be kind to yourself. Growth takes time, and you’re doing better than you think.",
+        "The only limits in life are the ones you set for yourself.",
+        "Embrace the small wins. They add up to big successes.",
+        "Don’t compare your journey to someone else’s. You’re on your own path.",
+        "The harder the battle, the sweeter the victory.",
+        "Believe in the process, even when you can’t see the results yet.",
+        "Sometimes, the best way to grow is to step out of your comfort zone.",
+      ];
+
+      // Select random advice from the list
+      const randomAdviceIndex = Math.floor(Math.random() * adviceList.length);
+      const selectedAdvice = adviceList[randomAdviceIndex];
+      setAdvice(selectedAdvice);
+
+      console.log("Selected Advice:", selectedAdvice);
     }
   }, [query]);
 
